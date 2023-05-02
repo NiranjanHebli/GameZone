0b896dd9f66e5ced9b180c9d5e7d2fa3a8a486f6
Niranjan <niranjanhebli77@gmail.com>
2023-05-02 20:12:20 +0530

    Basic implementation fo Hangman,Memory game,XO and RockpaperScissors added

---
+++ src/containers/Hangman/Hangman.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import "./Hangman.css";
+
+// const words = ['javascript', 'react', 'node', 'angular', 'vue'];
+const words = [
+  "html",
+  "css",
+  "javascript",
+  "typescript",
+  "react",
+  "angular",
+  "vue",
+  "node",
+  "express",
+  "mongodb",
+  "mysql",
+  "php",
+  "python",
+  "django",
+  "ruby",
+  "rails",
+  "sass",
+  "less",
+  "webpack",
+  "babel",
+];
+
+const Hangman = () => {
+  const [guess, setGuess] = useState("");
+  const [word, setWord] = useState(getRandomWord());
+  const [hiddenWord, setHiddenWord] = useState(generateHiddenWord(word));
+  const [guessesLeft, setGuessesLeft] = useState(6);
+  const [gameOver, setGameOver] = useState(false);
+
+  function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+  }
+
+  function generateHiddenWord(word) {
+    return word.replace(/\w/g, "-");
+  }
+
+  function handleGuess(e) {
+    e.preventDefault();
+
+    if (!guess) {
+      return;
+    }
+
+    if (word.includes(guess)) {
+      setHiddenWord(
+        hiddenWord
+          .split("")
+          .map((char, index) => {
+            if (word[index] === guess) {
+              return guess;
+            }
+            return char;
+          })
+          .join("")
+      );
+    } else {
+      setGuessesLeft(guessesLeft - 1);
+    }
+
+    setGuess("");
+  }
+
+const  handleReset=()=> {
+    window.location.reload()
+  }
+
+  if (guessesLeft <= 0) {
+    return (
+      <div className="hangman">
+        <h1>Game Over!</h1>
+        <p>The word was: {word}</p>
+        <button onClick={handleReset}>Play Again</button>
+      </div>
+    );
+  }
+
+  if (!hiddenWord.includes("-")) {
+    return (
+      <div className="hangman">
+        <h1>You Win!</h1>
+        <p>The word was: {word}</p>
+        <button onClick={handleReset}>Play Again</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="hangman">
+      <h1>Hangman</h1>
+      <p>Guesses Left: {guessesLeft}</p>
+      <p>Word: {hiddenWord}</p>
+      <form onSubmit={handleGuess}>
+        <input
+          type="text"
+          value={guess}
+          onChange={(e) => setGuess(e.target.value)}
+        />
+        <button type="submit">Guess</button>
+      </form>
+    </div>
+  );
+};
+
+export default Hangman;
---
+++ src/containers/MemoryGame/MemoryGame.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+import "./MemoryGame.css";
+const CARD_VALUES = ["A", "B", "C", "D", "E", "F", "G", "H"];
+const CARD_PAIRS = CARD_VALUES.length;
+const CARD_BACK = "X";
+
+const shuffleArray = (arr) => {
+  let i, j, temp;
+  for (i = arr.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
+};
+const generateCards = () => {
+  const cardPairs = [];
+  for (let i = 0; i < CARD_PAIRS; i++) {
+    cardPairs.push(CARD_VALUES[i]);
+    cardPairs.push(CARD_VALUES[i]);
+  }
+  return shuffleArray(cardPairs).map((value) => ({ value, flipped: false }));
+};
+
+const MemoryGame = () => {
+  const [cards, setCards] = useState(generateCards());
+  const [selectedCards, setSelectedCards] = useState([]);
+  useEffect(() => {
+    if (selectedCards.length !== 2) {
+      return;
+    }
+
+    const [card1Index, card2Index] = selectedCards;
+    const [card1, card2] = [cards[card1Index], cards[card2Index]];
+
+    if (card1.value === card2.value) {
+      const updatedCards = [...cards];
+      updatedCards[card1Index].flipped = true;
+      updatedCards[card2Index].flipped = true;
+      setCards(updatedCards);
+      setSelectedCards([]);
+    } else {
+        const updatedCards = [...cards];
+        updatedCards[card1Index].flipped = false;
+        updatedCards[card2Index].flipped = false;
+        setCards(updatedCards);
+        setSelectedCards([]);
+    }
+  }, [cards, selectedCards]);
+  const handleCardClick = (index) => {
+    const card = cards[index];
+
+    if (card.flipped) {
+      return;
+    }
+
+    const updatedCards = [...cards];
+    updatedCards[index].flipped = true;
+    setCards(updatedCards);
+
+    setSelectedCards([...selectedCards, index]);
+  };
+
+  return (<>
+  
+    <div className="memory-game">
+
+      <div className="board">
+        {cards.map((card, index) => (
+          <div
+            key={index}
+            className={`card ${card.flipped ? "flipped" : ""}`}
+            onClick={() => handleCardClick(index)}
+          >
+
+            {card.flipped ? card.value : CARD_BACK}
+          </div>
+
+        ))}
+
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default MemoryGame;
